d30963e77143637fe002decded9640d0dde462b5
Yared Endale <yared39@gmail.com>
2023-02-17 05:52:33 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include < stdio.h>
+/**
+ * main - A program print a line
+ * Return : 0 (done!)
+ * int main(void)
+ * {
+ * puts("/"Programming is like building a multilingual puzzle*;
+ * return 0)
+ *
