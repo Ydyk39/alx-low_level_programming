144c30ef4cd16fae65afe680be5a36a25e637f93
myusername <yared39@gmail.com>
2023-02-20 13:11:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,3 +7,10 @@
  * puts("/"Programming is like building a multilingual puzzle*);
  * return (0);
  *}
+ */
+int main(void)
+{
+puts("\" programming is like building a multilingual puzzle "");
+return (0);
+}
+
